2e7f3eee124e1624dc5502d24330b301dbf50eed
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-24 18:08:10 +0000

    Set up requirements

--- lib/recent_rental_listings.rb
+++ lib/recent_rental_listings.rb
@@ -1,5 +1,10 @@
-require "recent_rental_listings/version"
-
 module RecentRentalListings
   # Your code goes here...
 end
+
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+
+require "recent_rental_listings/version"
