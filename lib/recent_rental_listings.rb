d5ce9072f69a06794143b046b4ab632f7352047b
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-24 22:13:05 +0000

    Add requires to recent_rental_listings

--- lib/recent_rental_listings.rb
+++ lib/recent_rental_listings.rb
@@ -9,3 +9,8 @@ require 'nokogiri'
 
 require_relative "recent_rental_listings/version"
 require_relative "recent_rental_listings/scraper"
+require_relative "recent_rental_listings/state"
+require_relative "recent_rental_listings/city"
+require_relative "recent_rental_listings/type"
+require_relative "recent_rental_listings/rental"
+require_relative "recent_rental_listings/cli"
