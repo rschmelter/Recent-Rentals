c9d63bb32e7598fdf250f97d2b1a5e980c975555
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-24 21:02:32 +0000

    Type. set up initialize

--- lib/recent_rental_listings/type.rb
+++ lib/recent_rental_listings/type.rb
@@ -1,5 +1,16 @@
 class RecentRentalListings::Type
+  attr_accessor :name, :url, :city
 
+  @@all = []
+
+  def initialize(name, url, city)
+    @name = name
+    @city = city
+    @url = url
+    city.add_rental_type(self)
+    @rentals = []
+    @@all << self
+  end
 
 
 
