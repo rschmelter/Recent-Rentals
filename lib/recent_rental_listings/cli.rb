00e5bf764cef950d24b47858e339a2375a897594
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-24 21:45:42 +0000

    CLI. start

--- lib/recent_rental_listings/cli.rb
+++ lib/recent_rental_listings/cli.rb
@@ -1,7 +1,16 @@
 class RecentRentalListings::CLI
 
-
-
+  def start
+    @states_array = []
+    @state_hash = HousingList::Scraper.new.make_states_cities
+    @state_hash.keys.each do |state|
+      state = HousingList::State.new(state)
+      @states_array << state
+    end
+    puts "Welcome!"
+    puts " "
+    show_states
+  end
 
 
 
