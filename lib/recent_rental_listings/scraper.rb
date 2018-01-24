647891fcabbe9b1755b22837985c89d45b920b53
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-24 18:36:41 +0000

    Scraper. scrape_states

--- lib/recent_rental_listings/scraper.rb
+++ lib/recent_rental_listings/scraper.rb
@@ -4,7 +4,16 @@ require 'nokogiri'
 
 class RecentRentalListings::Scraper
 
+  def scrape_states
 
+    @states = []
+    doc = Nokogiri::HTML(open('https://www.craigslist.org/about/sites'))
+    a = doc.css(".colmask").first
+      a.css(".box h4").each do |state|
+        @states << state.text
+      end
+      @states
+  end
 
 
 
