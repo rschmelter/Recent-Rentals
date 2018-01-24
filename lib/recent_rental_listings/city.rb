1f05164a040152295e17d5eaea21d2b334c1f87f
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-24 20:55:15 +0000

    City. attr_accessor and initialize

--- lib/recent_rental_listings/city.rb
+++ lib/recent_rental_listings/city.rb
@@ -1,6 +1,16 @@
 
 class RecentRentalListings::City
+  attr_accessor :name, :state, :url
 
+  @@all = []
+
+  def initialize(name, state, url)
+    @name = name
+    @state = state
+    @url = url
+    state.add_city(self)
+    @@all << self
+  end
 
 
 
